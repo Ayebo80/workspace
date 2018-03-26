54879c2cba3bdab209c290857fb0a50970e2d2f7
Ayebo80 <patrickmaurer@gmx.de>
2018-03-27 00:26:38 +0200

    bis VOR 6) Using Selenium Server
    bis VOR 6) Using Selenium Server

--- WebDriverDemo/src/main/java/com/pluralsight/WebDriverDemo.java
+++ WebDriverDemo/src/main/java/com/pluralsight/WebDriverDemo.java
@@ -1,7 +1,13 @@
 package com.pluralsight;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebDriverDemo {
 	
@@ -11,6 +17,31 @@ public class WebDriverDemo {
 		
 		//WebDriver driver = new FirefoxDriver();
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://pluralsight.com");
+		driver.get("http://www.google.com");
+		
+		WebElement searchField = driver.findElement(By.id("lst-ib"));
+		searchField.sendKeys("pluralisght");
+		//get rid of that Type-Ahead AJAX Call
+		searchField.submit();
+		
+		//Implicit Wait will wait for each Elements
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//Explicit Wait will only wait on a specified Element
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Bilder")));
+		
+		WebElement imagesLink = driver.findElements(By.linkText("Bilder")).get(0);
+		imagesLink.click();
+		
+		WebElement imageElement = driver.findElements(By.cssSelector("a[class = rg_l]")).get(0);
+		WebElement imageLink = imageElement.findElements(By.tagName("img")).get(0);
+		imageLink.click();
+		
+		//driver.getPageSource().contains("Whatever Sring you want to search for");
+		
+		//searchField.sendKeys(Keys.RETURN);
+		//WebElement searchBtn = driver.findElement(By.id("gsr"));
+		//searchBtn.click();
+				
 	}
 }
